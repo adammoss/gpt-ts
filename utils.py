c9389da62b50bc838f6bd733af77e693569122f4
Adam Moss <3430494+adammoss@users.noreply.github.com>
2024-04-05 11:33:41 +0100

    Update randint function to accept device parameter
    The randint function in train.py and utils.py has been updated to accept a 'device' parameter. This modification allows random integers to be generated and stored directly on the specified computing device, improving overall efficiency. It mostly impacts the operations where the device needs to be explicitly specified.

--- utils.py
+++ utils.py
@@ -25,10 +25,10 @@ def generate(model, n_positions, idx, max_new_tokens, static=None, temperature=1
     return idx
 
 
-def randint(low, high=None, size=None):
+def randint(low, high=None, size=None, device=None):
     if high is None:
         high = low
         low = 0
     if size is None:
         size = low.shape if isinstance(low, torch.Tensor) else high.shape
-    return torch.randint(2**63 - 1, size=size) % (high - low) + low
+    return torch.randint(2**63 - 1, size=size, device=device) % (high - low) + low
