Introduce randomness to loss calculation and fix saving issue
In train.py, a random factor was introduced to the loss calculation. Now, it alternates between last_logits/last_labels and sliced_logits/sliced_labels, based on a 50/50 chance. Meanwhile, in eval.py, a small syntax error that hindered saving the confusion matrix was fixed.

eval.py
@@ -106,7 +106,7 @@ def main(args):
         y_pred.append(torch.argmax(output.logits, dim=-1).squeeze(0)[-1].cpu().numpy())
     cm = confusion_matrix(y_true, y_pred)
     if args.output_dir is not None:
-        np.save(os.path.join(args.output_dir, 'cm.npy', cm))
+        np.save(os.path.join(args.output_dir, 'cm.npy'), cm)
 
 
 if __name__ == "__main__":

train.py
@@ -498,7 +498,10 @@ def main(args):
         optimizer.zero_grad(set_to_none=True)
         if args.task == "finetune_last_class":
             if args.last_label_only:
-                loss = F.cross_entropy(last_logits, last_labels, label_smoothing=args.label_smoothing)
+                if np.random.rand() < 0.5:
+                    loss = F.cross_entropy(last_logits, last_labels, label_smoothing=args.label_smoothing)
+                else:
+                    loss = F.cross_entropy(sliced_logits, sliced_labels, label_smoothing=args.label_smoothing)
             else:
                 loss = F.cross_entropy(sliced_logits, sliced_labels, label_smoothing=args.label_smoothing)
         else: