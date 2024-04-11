5ac06ec72e8b987e723475189ab7314fa51dd7e2
Adam Moss <3430494+adammoss@users.noreply.github.com>
2024-04-11 18:18:15 +0100

    Update prepare_plasticc.py

--- prepare_plasticc.py
+++ prepare_plasticc.py
@@ -334,7 +334,7 @@ def process(args):
         if args.format == "gp_sample":
             #train_sequences = load_gp_sample_sequences(df_train_meta, df_train, args.sample_interval)
             test_files = glob.glob(os.path.join("plasticc", args.test_file_pattern))
-            with multiprocessing.Pool(processes=3) as pool:
+            with multiprocessing.Pool(processes=args.num_workers) as pool:
                 test_sequences = pool.starmap(load_gp_sample_sequences,
                                               zip(["plasticc/plasticc_test_metadata.csv.gz"] * len(test_files),
                                                   test_files, [args.sample_interval] * len(test_files)))
