3bb87692e2548d96221ac9b58dacc5042df526b5
Adam Moss <3430494+adammoss@users.noreply.github.com>
2024-04-14 00:42:57 +0100

    Update test_block.py

--- tests/test_block.py
+++ tests/test_block.py
@@ -8,13 +8,15 @@ class TestCasualBlock(unittest.TestCase):
 
     def setUp(self):
         # Setup process before each test
-        self.n_embd = 128
-        self.n_head = 2
-        self.n_positions = 1024
+        self.n_embd = 2
+        self.n_head = 1
+        self.n_positions = 256
+        self.sequence_length = 4
         self.position_embedding = None
-        self.is_causal = True
+        self.is_causal = False
         self.block = gpt.Block(self.n_embd, self.n_head, self.n_positions, position_embedding=self.position_embedding,
                                is_causal=self.is_causal)
+        self.block.eval()
 
     def test_block_init(self):
         # Testing for initialization
@@ -29,38 +31,28 @@ class TestCasualBlock(unittest.TestCase):
 
     def test_forward(self):
         # Checking forward computation
-        x = torch.rand((1, 512, self.n_embd))  # assuming input tensor of shape [batch, seq_length, n_embd]
+        x = torch.rand((1, self.sequence_length, self.n_embd))  # input tensor of shape [batch, seq_length, n_embd]
 
         # Positive case: when attention_mask is None
-        output = self.block.forward(x)
-        self.assertEqual(output.shape, x.shape)
+        ##output = self.block.forward(x)
+        ##self.assertEqual(output.shape, x.shape)
 
-        # Negative case: when attention_mask is not None
-        attention_mask = torch.ones((1, 512))  # attention mask of shape [batch, seq_length]
+        # Negative case: when attention_mask is not None (all attended so outputs should be the same)
+        ##attention_mask = torch.ones((1, self.sequence_length))  # attention mask of shape [batch, seq_length]
+        ##output_mask = self.block.forward(x, attention_mask)
+        ##self.assertEqual(output.shape, x.shape)
+        ##self.assertTrue(torch.allclose(output, output_mask))
+
+        # Check attended output is same when non-attended tokens change (=0)
+        attention_mask = torch.ones((1, self.sequence_length))  # attention mask of shape [batch, seq_length]
+        attention_mask[0, int(self.sequence_length / 4):int(self.sequence_length / 2)] = 0
         output = self.block.forward(x, attention_mask)
-        self.assertEqual(output.shape, x.shape)
+        x[0, int(self.sequence_length / 4):int(self.sequence_length / 2), :] = 0
+        output_x0 = self.block.forward(x, attention_mask)
+        self.assertTrue(torch.allclose(output[0, :int(self.sequence_length / 4):, :],
+                                       output_x0[0, : int(self.sequence_length / 4):, :]))
+        self.assertTrue(torch.allclose(output[0, int(self.sequence_length / 2):, :],
+                                       output_x0[0, int(self.sequence_length / 2):, :]))
 
-        # Check attended tokens when masked (=0)
-        attention_mask[0, 256:] = 0
-        output_mask = self.block.forward(x, attention_mask)
-        self.assertEqual(output_mask.shape, x.shape)
-
-        self.assertTrue(torch.allclose(output[:, :256, :], output_mask[:, :256, :]))
-        self.assertFalse(torch.allclose(output[:, 256:, :], output_mask[:, 256:, :]))
-
-
-class TestBlock(TestCasualBlock):
-
-    def setUp(self):
-        # Setup process before each test
-        self.n_embd = 128
-        self.n_head = 2
-        self.n_positions = 1024
-        self.position_embedding = None
-        self.is_causal = False
-        self.block = gpt.Block(self.n_embd, self.n_head, self.n_positions, position_embedding=self.position_embedding,
-                               is_causal=self.is_causal)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        if __name__ == "__main__":
+            unittest.main()
